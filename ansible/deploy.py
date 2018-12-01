dbc70f3adc014140b1c4f662babf008fcf7a4be0
Michael Liao <askxuefeng@gmail.com>
2018-12-01 15:15:20 +0800

    update vars

--- ansible/deploy.py
+++ ansible/deploy.py
@@ -26,6 +26,8 @@ def main():
     run('ansible-playbook --version')
     for name in apps:
         run('ansible-playbook -i environments/%s/hosts.yml playbook.yml --extra-vars "profile=%s name=%s"' % (profile, profile, name))
+    if 'mq' in apps:
+        print('\n\x1b[6;30;47m%s\x1b[0m' % 'IMPORTANT: you have to start and init RocketMQ manually after you deploy mq.')
 
 if __name__ == '__main__':
     main()
